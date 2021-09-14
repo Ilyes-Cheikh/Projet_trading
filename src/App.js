f3d99eb28b687fee2cc68f2e840ec5d5dc8e739a
Ilyes-Cheikh <Cheikh.ilyes10@gmail.com>
2021-09-14 23:03:27 +0100

    resposive aboutus page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
     <>
       
       <Router>
-        <Navbar/>
         <Switch>
           <Route exact path="/"><Home/></Route>
           <Route exact path="/about"><About/></Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,17 @@ import OurTechnologies from '../components/OurTechnologies'
 import Test from '../components/test'
 import Footer from '../components/Footer'
 import News from '../components/news'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
     return (
         <>
-            <HomeHeader/>
-            <Test/>
-            <OurTechnologies/>
-            <News/>
-            <Footer/>
+            <Navbar />
+            <HomeHeader />
+            <Test />
+            <OurTechnologies />
+            <News />
+            <Footer />
         </>
     )
 }
